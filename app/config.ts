24d9e2e0c3471e9afd973ce5eaa546a202c046a5
Saurabh Shukla <ss6408367@gmail.com>
2025-03-04 11:29:23 +0530

    fixed the linkdin link

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,15 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
   title: "Saurabh Shukla",
-  name: "Sirius",
-  ogImage: "/opengraph-image.png",
-  description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+  name: "Saurabh Shukla",
+  ogImage: "/favicon.png",
+  description: "My personal porfolio",
 };
 
 export const socialLinks = {
   twitter: "https://x.com/saurabhisshukla",
   github: "https://github.com/skinnyiam",
   instagram: "https://www.instagram.com/saurabh_ishere/",
-  linkedin: "www.linkedin.com/in/saurabh-ishere",
+  linkedin: "https://www.linkedin.com/in/saurabh-ishere/",
   email: "mailto:saurabh@kroto.one",
 };
